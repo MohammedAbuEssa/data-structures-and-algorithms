7ad641df2fa004c4fa81b6412b5ff67b9ad1608e
Mohammad AbuEssa <78694579+MohammedAbuEssa@users.noreply.github.com>
2023-06-11 10:24:29 +0300

    Update LinkedList.js

--- linked-list/LinkedList.js
+++ linked-list/LinkedList.js
@@ -1,24 +1,61 @@
 "use strict";
-
-const Node = require("./Node");
-
-class Linkedlist {
+const Node = require("./Node.js");
+class LinkedList {
   constructor() {
     this.head = null;
-    // this.tail=null;
-    // this.size=;
   }
   append(value) {
-    const newNode = new Node(value);
+    let newNode = new Node(value);
+    // this.counter++;
     if (!this.head) {
       this.head = newNode;
       return this;
     }
-    let currentNode = this.head;
-    while (currentNode.next) {
-      currentNode = currentNode.next;
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
     }
-    currentNode.next = newNode;
+    current.next = newNode;
+    // this.tail=newNode
     return this;
   }
+  includeIn(value) {
+    if (value == this.head) {
+      return true;
+    }
+    let current = this.head;
+    while (current) {
+      if (value == current.value) {
+        return true;
+      } else if (current.next == null) {
+        return false;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+  stringCasting() {
+    let current = this.head;
+    let linkedlistStr = '"';
+    while (current) {
+      if (current.next === null) {
+        linkedlistStr += `{${current.value}}  -> NULL\"`;
+        return linkedlistStr;
+      }
+      linkedlistStr += `{${current.value}} -> `;
+      current = current.next;
+    }
+    return `"NULL"`;
+  }
+  addToHead(value) {
+    let newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
 }
+module.exports = LinkedList;
