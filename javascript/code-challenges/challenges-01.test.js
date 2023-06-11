0c721b3af0baa5fe10e3dbbdfa6d053274daa2b1
MohammedAbuEssa <mabuissa5@gmail.com>
2023-06-11 16:45:39 +0000

    Finish foreach

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -94,11 +94,16 @@ Return the modified array.
 
 const addValues = (arr, value) => {
   // Solution code here...
-  const newArr = [...arr, value];
+  arr.push(value);
 };
 
 const addNumbers = (num, arr, times, callback) => {
   // Solution code here...
+
+  for (let i = 0; i < times; i++) {
+    callback(arr, num);
+  }
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -148,16 +153,22 @@ Return the resulting output array.
 
 const fizzbuzz = (arr) => {
   // Solution code here...
-  const newArr = [];
-  arr.forEach((element) => {
-    if (element % 3 === 0) {
-      newArr.push("Fizz");
-    } else if (element % 5 === 0) {
-      newArr.push("Buzz");
-    } else if (element % 5 === 0 && element % 3 === 0) {
-      newArr.push("Fizz Buzz");
+
+  const output = [];
+
+  arr.forEach((number) => {
+    if (number % 3 === 0 && number % 5 === 0) {
+      output.push("Fizz Buzz");
+    } else if (number % 3 === 0) {
+      output.push("Fizz");
+    } else if (number % 5 === 0) {
+      output.push("Buzz");
+    } else {
+      output.push(number);
     }
   });
+
+  return output;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -231,7 +242,7 @@ describe("Testing challenge 6", () => {
   });
 });
 
-xdescribe("Testing challenge 7", () => {
+describe("Testing challenge 7", () => {
   const inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
   test("It should print out messages or numbers", () => {
