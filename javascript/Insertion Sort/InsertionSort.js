b5656d42b0c745eeb6a207bd5fdc3c35d824c976
MohammedAbuEssa <mabuissa5@gmail.com>
2023-08-14 13:42:09 +0000

    Done

---
+++ javascript/Insertion Sort/InsertionSort.js
@@ -0,0 +1,24 @@
+function insert(sorted, value) {
+  let i = 0;
+  while (value > sorted[i]) {
+    i++;
+  }
+  while (i < sorted.length) {
+    const temp = sorted[i];
+    sorted[i] = value;
+    value = temp;
+    i++;
+  }
+  sorted.push(value);
+}
+
+function insertionSort(input) {
+  const sorted = [input[0]];
+  for (let i = 1; i < input.length; i++) {
+    insert(sorted, input[i]);
+  }
+  return sorted;
+}
+
+
+module.exports = insertionSort;
